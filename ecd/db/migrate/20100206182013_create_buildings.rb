7d4a76a1e22b47f3fcc8c3ee4690c2d81372b1ff
Trevor Chapman <chapmant@onid.oregonstate.edu>
2010-02-08 11:25:47 -0800

    Classes are now in 20100206182013_create_buildings.rb, but can't seem to get it to create it to the sqlite3 db aside from table buildings.

--- ecd/db/migrate/20100206182013_create_buildings.rb
+++ ecd/db/migrate/20100206182013_create_buildings.rb
@@ -31,3 +31,31 @@ class CreateDatas < ActiveRecord::Migration
     drop_table :datas
   end
 end
+
+class CreateBuildingTypes < ActiveRecord::Migration
+  def self.up
+    create_table :building_types, :primary_key => :building_type_id do |t|
+      t.integer :building_type_id
+      t.string :name
+      t.timestamps
+    end
+  end
+  
+  def self.down
+    drop_table :building_types
+  end
+end
+
+class CreateMeterTypes < ActiveRecord:Migration
+  def self.up
+    create_table :metertypes, :primary_key => :meter_type_id do |t|
+      t.integer :meter_type_id
+      t.string :name
+      t.string :unit
+    end
+  end
+
+  def self.down
+    drop_table :metertypes
+  end
+end
