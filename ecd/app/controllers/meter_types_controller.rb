90c4f8c1cc6b6e711f52f40609fe2c3f6b736878
Richard Tracy <thebigcheeze@gmail.com>
2010-02-13 17:01:23 -0800

    added redirection test

--- ecd/app/controllers/meter_types_controller.rb
+++ ecd/app/controllers/meter_types_controller.rb
@@ -85,6 +85,6 @@ class MeterTypesController < ApplicationController
   
   def getdata
 	@meter_type = MeterType.find(params[:id])
-	redirect_to(@meter_type)  
+	redirect_to("http://128.193.122.20/setup/devicexml.cgi?ADDRESS=" + @meter_type.Mod_Bus_Address + "&TYPE=DATA")
   end
 end
