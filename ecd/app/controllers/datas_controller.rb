82d9f3fbd18c67acb305f5e6906b462dbe7d32f5
Trevor Chapman <chapmant@access.cws.oregonstate.edu>
2010-02-14 17:26:21 -0800

    parsing

--- ecd/app/controllers/datas_controller.rb
+++ ecd/app/controllers/datas_controller.rb
@@ -1,26 +1,26 @@
 class DatasController < ApplicationController
 
   def show
-	respond_to do |format|
-      format.html
-	end
-  end
+#	respond_to do |format|
+ #     format.html
+#	end
+#  end
 
-  def update
+ # def update
     require 'rexml/document'
-    include REXML
-    Meter do |meter|
-	    @addr = meter.modbus_address
+   # include REXML
+    Meter.all do |metertron|
+	    @addr = metertron.modbus_address
 	    @full_addr = "http://128.193.122.20/setup/devicexml.cgi?ADDRESS=" + @addr.to_s() + "&TYPE=DATA"
 	    file = File.new(full_addr)
 	    doc = Document.new(file)
-	    root = doc.root.DAS.devices.device.records.record
+	    root = doc.root.DAS.devices.device.records.record.point
 	    puts root
 
     end
-    redirect_to(Buildings)
-    respond_to do |format|
-	    format.html
-    end
+    redirect_to(root.elements[1])
+    #respond_to do |format|
+#	    format.html
+   # end
   end
 end
