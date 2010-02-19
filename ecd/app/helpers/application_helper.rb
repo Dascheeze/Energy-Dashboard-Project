40d2ba9ec5776b0384b8065cb149f1b504d2f5b5
Richard Tracy <thebigcheeze@gmail.com>
2010-02-18 18:56:30 -0800

    Added helper functions to application_helper

--- ecd/app/helpers/application_helper.rb
+++ ecd/app/helpers/application_helper.rb
@@ -1,3 +1,28 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def getURL(command)
+    require 'rubygems'
+    require 'net/http'
+    $username = 'admin'
+    $password = 'admin'
+    # Open an HTTP connection to 
+    ret = Net::HTTP.start('128.193.122.20')
+
+    # Depending on the request type, create either
+    # an HTTP::Get or HTTP::Post object
+
+    req = Net::HTTP::Get.new(command)
+
+    # Set up the authentication and
+    # make the request
+    req.basic_auth( $username, $password )
+    res = ret.request(req)
+
+    # Return the request body
+    return res.body
+  end
+  
+  def getMeterXML(meterAddress)
+    return getURL('/setup/devicexml.cgi?ADDRESS=' + meterAddress + '&TYPE=DATA')
+  end
 end
