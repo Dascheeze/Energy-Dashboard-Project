39f12cbcf52c5659f40efa3999b39212e51b65ab
Richard Tracy <thebigcheeze@gmail.com>
2010-02-20 15:05:21 -0800

    added test data add

--- ecd/app/controllers/meters_controller.rb
+++ ecd/app/controllers/meters_controller.rb
@@ -11,10 +11,8 @@ class MetersController < ApplicationController
   end
 
   def refresh
-    @meters = Meter.all
-	all_meters = @meters
-	all_meters do |meter|
-	  parse_xml meter.modbus_address meter.id
+	Meter.all do |meter|
+	  parse_xml(meter.modbus_address, meter.id)
 	end
 	respond_to do |format|
 		format.html
