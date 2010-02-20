9e4455ca326635aa0325120a24ca3860cb5e5231
Richard Tracy <thebigcheeze@gmail.com>
2010-02-20 14:29:43 -0800

    moved refresh higher up

--- ecd/app/controllers/meters_controller.rb
+++ ecd/app/controllers/meters_controller.rb
@@ -12,8 +12,9 @@ class MetersController < ApplicationController
 
   
   def refresh
-    meters = Meter.all
-	meters do |meter|
+    @meters = Meter.all
+	all_meters = @meters
+	all_meters do |meter|
 	  parse_xml meter.modbus_address meter.id
 	end
 	respond_to do |format|
