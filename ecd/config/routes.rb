568ec6228a8ac905038000d3767c32ec3e274302
Richard Tracy <thebigcheeze@gmail.com>
2010-02-26 00:42:50 -0800

    fixed routes

--- ecd/config/routes.rb
+++ ecd/config/routes.rb
@@ -53,5 +53,5 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
   
-  map.root :welcome => "welcome"
+  map.root :controller => "welcome"
 end
