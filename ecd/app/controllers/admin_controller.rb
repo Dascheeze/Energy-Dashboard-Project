394bb66d9ef83dec0f4b1849dcb9fd3fb909b8a9
Richard Tracy <thebigcheeze@gmail.com>
2010-04-25 18:56:16 -0700

    added test auth code

--- ecd/app/controllers/admin_controller.rb
+++ ecd/app/controllers/admin_controller.rb
@@ -1,7 +1,19 @@
 class AdminController < ApplicationController
+  USER_NAME, PASSWORD = "admin", "admin"
+  
+  before_filter :authenticate  
+  
   def index
     respond_to do |format|
       format.html # index.html.erb
     end
   end
+  
+  private
+    def authenticate
+      authenticate_or_request_with_http_basic do |user_name, password|
+        user_name == USER_NAME && password == PASSWORD
+      end
+    end
+  
 end
