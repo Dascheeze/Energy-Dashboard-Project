d4ca96c601e1284e6dbc1561f22e9bfbb312a48d
Trevor Chapman <chapmant@access.cws.oregonstate.edu>
2010-05-08 16:44:21 -0700

    Fixing comments

--- ecd/app/helpers/application_helper.rb
+++ ecd/app/helpers/application_helper.rb
@@ -4,33 +4,23 @@ module ApplicationHelper
   class	Item
     attr_reader :amount, :date
 	attr_writer :date
-	
+
 	def amount=(a)
 	  @amount = sprintf('%.2f', a).to_f
 	end
-	
+
   end
-  
+
   class Bounds
     attr_reader :min_amount, :min_time, :max_amount, :max_time
 	attr_writer :min_amount, :min_time, :max_amount, :max_time
   end
-  
+
   def points_between_dates(data_set_id, start_date, end_date)
-    #return_table = "<table>"
-    #list_sets = DataSet.find(:all, :conditions => { :created_at => start_date..end_date, :id => data_set_id})
-    #list_sets.each do |set|
     list_points = DataPoint.find(:all, :conditions => { :created_at => (start_date.gmtime)..(end_date.gmtime), :data_set_id => data_set_id })
-    #return_table = return_table + "<tr>"
-    #list_points.each do |data_points|
-    #  return_table = return_table + "<td>" + data_points.amount.to_s + "</td>"
-    #end
-    #return_table = return_table + "</tr>"
-    #end
-    #return_table = return_table + "</table>"
     return list_points
   end
-  
+
   def data_to_array(list_points, time_interval = 1.hour)
     duration_of_pull = 15.minutes
     data_array = Array.new
@@ -57,7 +47,7 @@ module ApplicationHelper
     return data_array
   end
 
-  
+
   def get_bounds(data_array)
 	bounds = Bounds.new
     bounds.max_amount=0
@@ -65,18 +55,18 @@ module ApplicationHelper
     bounds.min_amount=0
     bounds.min_time=0
     i=0
-    
+
     data_array.each do |point|
       if(i==0)
         bounds.min_amount = point.amount
         bounds.min_time = point.date
       end
-      
+
       if(bounds.max_amount < point.amount)
         bounds.max_amount = point.amount
         bounds.max_time = point.date
       end
-	  
+
 	  if(bounds.min_amount > point.amount)
         bounds.min_amount = point.amount
         bounds.min_time = point.date
@@ -85,7 +75,7 @@ module ApplicationHelper
     end
     return bounds
   end
-  
+
   def real_diff (list_points, time_interval = 1.hour)
 	  duration_of_pull = 15.minutes
 	  data_array = Array.new
