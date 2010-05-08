f540384aeaa13c8ca654dfe1bc919696a4481720
Richard Tracy <thebigcheeze@gmail.com>
2010-05-08 14:14:16 -0700

    perhaps it's an off by one error again...

--- ecd/app/controllers/data_sets_controller.rb
+++ ecd/app/controllers/data_sets_controller.rb
@@ -134,12 +134,12 @@ class DataSetsController < ApplicationController
     logger.debug params
     
     if params[:dateform] and params[:dateform][:start_date]
-	    @start_time = Time.parse(params[:dateform][:start_date]).midnight.localtime
+	    @start_time = Time.parse(params[:dateform][:start_date]).localtime.midnight
     else
-      @start_time = Time.now.midnight.localtime - 30.day
+      @start_time = Time.now.localtime.midnight - 29.day
     end
       
-    @endTime = @start_time + 31.day
+    @endTime = @start_time + 30.day
     
     logger.debug @data_set_id
     
